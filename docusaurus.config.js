33282268977165f208eb0bc5aded119a27ba306b
comboner <armin.zaugg@gmail.com>
2023-01-14 09:37:20 +0100

    Next time someone asks you how to fix an infinite loop, remember this commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'comboner', // Usually your GitHub org/user name.
-  projectName: 'documentation', // Usually your repo name.
+  projectName: 'gh-wiki', // Usually your repo name.
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
