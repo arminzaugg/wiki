1c566047ebf44a669648952052e80f703efd1d8e
comboner <armin.zaugg@gmail.com>
2023-01-16 17:49:33 +0100

    Your commit is writing checks your merge can't cash.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Wiki',
   tagline: 'Documentation just got cool again',
   url: 'https://comboner.github.io',
-  baseUrl: '/gh-wiki/',
+  baseUrl: '/docs/intro',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
